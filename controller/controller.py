a5671e1638d77e9b8d1136e6d3b100768603051a
b3rnerd <einsteinbernard3000@gmail.com>
2019-04-05 20:31:38 +0300

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -1,10 +1,13 @@
+#Import the models module from models package
 from models import models
 
+#the template to be rendered
 html = "<!DOCTYPE html><html><head lang=\"en\"><meta charset=\"UTF-8\"><title>GOT Character Profile</title><style>" \
        "table, th, td {border: 1px solid black;}</style></head><body><h1>GOT Character Profiles</h1><table><tr><th>Name</th><th>District</th><" \
        "th>Mother's Name</th><th>Father's Name</th><th>Date Added</th><th>Episodes Per Season</th><th>Died in Se" \
        "ason</th></tr>%s</table></body></html>"
 
+#the controllers modules to handle data from the models
 user_appearance = []
 for character in models.characters:
     name = character.get('name')
@@ -29,6 +32,7 @@ for character in models.characters:
             character.get('Death Season'))
     page_data.append(data)
 
+#file operations
 page = open('index.html', 'w')
 page.write(html % (''.join(page_data)))
 page.close()
